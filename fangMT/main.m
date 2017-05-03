0b5131fff459ab403f1829023d9ba091822df120
chengrong03 <chengrong03@meituan.com>
2017-05-03 15:49:47 +0800

    Initial Commit

---
+++ fangMT/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  fangMT
+//
+//  Created by Rong on 2017/5/3.
+//  Copyright © 2017年 Rong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
