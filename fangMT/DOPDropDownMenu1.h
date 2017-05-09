2638eae6d3b264ab19be08c649eab8f614a5d01c
chengrong03 <chengrong03@meituan.com>
2017-05-09 09:03:33 +0800

    下拉筛选框

---
+++ fangMT/DOPDropDownMenu1.h
@@ -0,0 +1,147 @@
+//
+//  DOPDropDownMenu1.h
+//  fangMT
+//
+//  Created by Rong on 2017/5/9.
+//  Copyright © 2017年 Rong. All rights reserved.
+//
+
+//
+//  DOPDropDownMenu.h
+//  DOPDropDownMenuDemo
+//
+//  Created by weizhou on 9/26/14.
+//  Modify by tanyang on 20/3/15.
+//  Copyright (c) 2014 fengweizhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DOPIndexPath1 : NSObject
+
+@property (nonatomic, assign) NSInteger column;
+@property (nonatomic, assign) NSInteger row;
+@property (nonatomic, assign) NSInteger item;
+- (instancetype)initWithColumn:(NSInteger)column row:(NSInteger)row;
+// default item = -1
++ (instancetype)indexPathWithCol:(NSInteger)col row:(NSInteger)row;
++ (instancetype)indexPathWithCol:(NSInteger)col row:(NSInteger)row item:(NSInteger)item;
+@end
+
+@interface DOPBackgroundCellView1 : UIView
+
+@end
+
+#pragma mark - data source protocol
+@class DOPDropDownMenu1;
+
+@protocol DOPDropDownMenuDataSource <NSObject>
+
+@required
+
+/**
+ *  返回 menu 第column列有多少行
+ */
+- (NSInteger)menu:(DOPDropDownMenu1 *)menu numberOfRowsInColumn:(NSInteger)column;
+
+/**
+ *  返回 menu 第column列 每行title
+ */
+- (NSString *)menu:(DOPDropDownMenu1 *)menu titleForRowAtIndexPath:(DOPIndexPath1 *)indexPath;
+
+@optional
+/**
+ *  返回 menu 有多少列 ，默认1列
+ */
+- (NSInteger)numberOfColumnsInMenu:(DOPDropDownMenu1 *)menu;
+
+// 新增 返回 menu 第column列 每行image
+- (NSString *)menu:(DOPDropDownMenu1 *)menu imageNameForRowAtIndexPath:(DOPIndexPath1 *)indexPath;
+
+// 新增 detailText ,right text
+- (NSString *)menu:(DOPDropDownMenu1 *)menu detailTextForRowAtIndexPath:(DOPIndexPath1 *)indexPath;
+
+/** 新增
+ *  当有column列 row 行 返回有多少个item ，如果>0，说明有二级列表 ，=0 没有二级列表
+ *  如果都没有可以不实现该协议
+ */
+- (NSInteger)menu:(DOPDropDownMenu1 *)menu numberOfItemsInRow:(NSInteger)row column:(NSInteger)column;
+
+/** 新增
+ *  当有column列 row 行 item项 title
+ *  如果都没有可以不实现该协议
+ */
+- (NSString *)menu:(DOPDropDownMenu1 *)menu titleForItemsInRowAtIndexPath:(DOPIndexPath1 *)indexPath;
+
+// 新增 当有column列 row 行 item项 image
+- (NSString *)menu:(DOPDropDownMenu1 *)menu imageNameForItemsInRowAtIndexPath:(DOPIndexPath1 *)indexPath;
+// 新增
+- (NSString *)menu:(DOPDropDownMenu1 *)menu detailTextForItemsInRowAtIndexPath:(DOPIndexPath1 *)indexPath;
+
+@end
+
+#pragma mark - delegate
+@protocol DOPDropDownMenuDelegate <NSObject>
+@optional
+
+/**
+ *  点击代理，点击了第column
+ */
+- (void)menu:(DOPDropDownMenu1 *)menu didSelectColumn:(NSUInteger)column;
+
+/**
+ *  点击代理，点击了第column 第row 或者item项，如果 item >=0
+ */
+- (void)menu:(DOPDropDownMenu1 *)menu didSelectRowAtIndexPath:(DOPIndexPath1 *)indexPath;
+
+/** 新增
+ *  return nil if you don't want to user select specified indexpath
+ *  optional
+ */
+- (NSIndexPath *)menu:(DOPDropDownMenu1 *)menu willSelectRowAtIndexPath:(DOPIndexPath1 *)indexPath;
+
+@end
+
+#pragma mark - interface
+@interface DOPDropDownMenu1 : UIView <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, weak) id <DOPDropDownMenuDataSource> dataSource;
+@property (nonatomic, weak) id <DOPDropDownMenuDelegate> delegate;
+
+@property (nonatomic, assign) UITableViewCellStyle cellStyle; // default value1
+@property (nonatomic, strong) UIColor *indicatorColor;      // 三角指示器颜色
+@property (nonatomic, strong) UIColor *textColor;           // 文字title颜色
+@property (nonatomic, strong) UIColor *textSelectedColor;   // 文字title选中颜色
+@property (nonatomic, strong) UIColor *detailTextColor;     // detailText文字颜色
+@property (nonatomic, strong) UIFont *detailTextFont;       // font
+@property (nonatomic, strong) UIColor *separatorColor;      // 分割线颜色
+@property (nonatomic, assign) NSInteger fontSize;           // 字体大小
+// 当有二级列表item时，点击row 是否调用点击代理方法
+@property (nonatomic, assign) BOOL isClickHaveItemValid;
+
+@property (nonatomic, getter=isRemainMenuTitle) BOOL remainMenuTitle; // 切换条件时是否更改menu title
+@property (nonatomic, strong) NSMutableArray  *currentSelectRowArray; // 恢复默认选项用
+/**
+ *  the width of menu will be set to screen width defaultly
+ *
+ *  @param origin the origin of this view's frame
+ *  @param height menu's height
+ *
+ *  @return menu
+ */
+- (instancetype)initWithOrigin:(CGPoint)origin andHeight:(CGFloat)height;
+
+// 获取title
+- (NSString *)titleForRowAtIndexPath:(DOPIndexPath1 *)indexPath;
+
+// 重新加载数据
+- (void)reloadData;
+
+// 创建menu 第一次显示 不会调用点击代理，这个手动调用
+- (void)selectDefalutIndexPath;
+
+- (void)selectIndexPath:(DOPIndexPath1 *)indexPath; // 默认trigger delegate
+
+- (void)selectIndexPath:(DOPIndexPath1 *)indexPath triggerDelegate:(BOOL)trigger; // 调用代理
+@end
+
--- fangMT/HomePageViewController.m
+++ fangMT/HomePageViewController.m
@@ -15,9 +15,11 @@
 #import "MJChiBaoZiHeader.h"
 #import "UIView+Border.h"
 
+#import "DOPDropDownMenu1.h"
+
 const static NSString *kTableViewCellIdentifier = @"tableViewCellIdentifier";
 
-@interface HomePageViewController ()<UITableViewDelegate, UITableViewDataSource, FilterConditionLineViewDelegate>
+@interface HomePageViewController ()<UITableViewDelegate, UITableViewDataSource, FilterConditionLineViewDelegate, DOPDropDownMenuDelegate, DOPDropDownMenuDataSource>
 
 @property (nonatomic, strong) HomePageTableHeaderView *headerView;
 @property (nonatomic, strong) FilterConditionLineView *filterConditionLineView;
@@ -25,6 +27,18 @@ const static NSString *kTableViewCellIdentifier = @"tableViewCellIdentifier";
 
 @property (nonatomic, strong) MerchantInfoListViewModel *merchantInfoListViewModel;
 @property (nonatomic, copy) NSMutableArray *merchantInfoListArray;
+
+@property (nonatomic, strong) DOPDropDownMenu1 *dropDownMenu;
+
+// dropDownMenu数据
+@property (nonatomic, strong) NSArray *classifys;
+@property (nonatomic, strong) NSArray *cates;
+@property (nonatomic, strong) NSArray *movices;
+@property (nonatomic, strong) NSArray *hostels;
+
+@property (nonatomic, strong) NSArray *areas;
+@property (nonatomic, strong) NSArray *sorts;
+@property (nonatomic, strong) NSArray *filters;
 
 @end
 
@@ -59,6 +73,8 @@ const static NSString *kTableViewCellIdentifier = @"tableViewCellIdentifier";
     
     [self.tableView.mj_header beginRefreshing];
     [self.tableView.mj_footer beginRefreshing];
+    
+    [self mockFiterData];
 
 }
 
@@ -109,6 +125,7 @@ const static NSString *kTableViewCellIdentifier = @"tableViewCellIdentifier";
 - (FilterConditionLineView *)filterConditionLineView {
     if (!_filterConditionLineView) {
         _filterConditionLineView = [[FilterConditionLineView alloc] initWithFrame:CGRectMake(0, 0, ScreenWidth, 39.)];
+        
         NSArray *conditionsCatergory = @[@"全部",
                                          @"附近",
                                          @"智能排序",
@@ -128,6 +145,26 @@ const static NSString *kTableViewCellIdentifier = @"tableViewCellIdentifier";
     return _filterConditionLineView;
 }
 
+- (DOPDropDownMenu1 *)dropDownMenu {
+    if (!_dropDownMenu) {
+        _dropDownMenu = [[DOPDropDownMenu1 alloc] initWithOrigin:CGPointMake(0, 64) andHeight:44.];
+        _dropDownMenu.dataSource = self;
+        _dropDownMenu.delegate = self;
+        
+        //当下拉菜单收回时的回调，用于网络请求新的数据
+//        _dropDownMenu.finishedBlock=^(DOPIndexPath *indexPath){
+//            if (indexPath.item >= 0) {
+//                NSLog(@"收起:点击了 %ld - %ld - %ld 项目",indexPath.column,indexPath.row,indexPath.item);
+//            }else {
+//                NSLog(@"收起:点击了 %ld - %ld 项目",indexPath.column,indexPath.row);
+//            }
+//        };
+
+    }
+   
+    return  _dropDownMenu;
+}
+
 #pragma mark - UITableviewDelegate
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -139,7 +176,7 @@ const static NSString *kTableViewCellIdentifier = @"tableViewCellIdentifier";
 
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
     
-    return 39.;
+    return 44.;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
@@ -149,7 +186,7 @@ const static NSString *kTableViewCellIdentifier = @"tableViewCellIdentifier";
 
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
     
-    return self.filterConditionLineView;
+    return self.dropDownMenu;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -179,7 +216,7 @@ const static NSString *kTableViewCellIdentifier = @"tableViewCellIdentifier";
     return cell;
 }
 
-#pragma mark -- helper methods
+#pragma mark - helper methods
 
 - (void)loadMoreData {
     // 2.模拟2秒后刷新表格UI（真实开发中，可以移除这段gcd代码）
@@ -220,6 +257,16 @@ const static NSString *kTableViewCellIdentifier = @"tableViewCellIdentifier";
     self.tableView.mj_footer = footer;
 }
 
+- (void)mockFiterData {
+    self.classifys = @[@"美食",@"今日新单",@"电影",@"酒店"];
+    self.cates = @[@"自助餐",@"快餐",@"火锅",@"日韩料理",@"西餐",@"烧烤小吃"];
+    self.movices = @[@"内地剧",@"港台剧",@"英美剧"];
+    self.hostels = @[@"经济酒店",@"商务酒店",@"连锁酒店",@"度假酒店",@"公寓酒店"];
+    self.areas = @[@"全城",@"芙蓉区",@"雨花区",@"天心区",@"开福区",@"岳麓区"];
+    self.sorts = @[@"默认排序",@"离我最近",@"好评优先",@"人气优先",@"最新发布"];
+    self.filters = @[@"默认排序",@"离我最近",@"好评优先",@"人气优先",@"最新发布"];
+}
+
 #pragma mark - FilterConditionLineViewDelegate
 
 - (void)clickFilterButtonWithBlock:(void (^)(void))block {
@@ -229,6 +276,141 @@ const static NSString *kTableViewCellIdentifier = @"tableViewCellIdentifier";
     }];
     
     block();
+}
+
+#pragma mark - DOPDropDownMenuDataSource
+
+/**
+ *  返回 menu 第column列有多少行
+ */
+- (NSInteger)menu:(DOPDropDownMenu1 *)menu numberOfRowsInColumn:(NSInteger)column {
+    NSUInteger rows = 1;
+    switch (column) {
+        case 0:
+            rows = [self.classifys count];
+            break;
+        case 1:
+            rows = [self.areas count];
+            break;
+        case 2:
+            rows = [self.sorts count];
+            break;
+        case 3:
+            rows = [self.sorts count];
+            break;
+        default:
+            break;
+    }
+    
+    return rows;
+}
+
+/**
+ *  返回 menu 第column列 每行title
+ */
+- (NSString *)menu:(DOPDropDownMenu1 *)menu titleForRowAtIndexPath:(DOPIndexPath1 *)indexPath {
+    NSString *title = @"";
+    switch (indexPath.column) {
+        case 0:
+            title = self.classifys[indexPath.row];
+            break;
+        case 1:
+            title = self.areas[indexPath.row];
+            break;
+        case 2:
+            title = self.sorts[indexPath.row];
+            break;
+        case 3:
+            title = self.sorts[indexPath.row];
+            break;
+        default:
+            break;
+    }
+    
+    return title;
+}
+
+/**
+ *  返回 menu 有多少列 ，默认1列
+ */
+- (NSInteger)numberOfColumnsInMenu:(DOPDropDownMenu1 *)menu {
+    return 4;
+}
+
+// 新增 返回 menu 第column列 每行image
+- (NSString *)menu:(DOPDropDownMenu1 *)menu imageNameForRowAtIndexPath:(DOPIndexPath1 *)indexPath {
+    if (indexPath.column == 0 || indexPath.column == 1) {
+        return [NSString stringWithFormat:@"ic_filter_category_%ld",indexPath.row];
+    }
+    
+    return nil;
+}
+
+- (NSInteger)menu:(DOPDropDownMenu1 *)menu numberOfItemsInRow:(NSInteger)row column:(NSInteger)column
+{
+    if (column == 0) {
+        if (row == 0) {
+            return self.cates.count;
+        } else if (row == 2){
+            return self.movices.count;
+        } else if (row == 3){
+            return self.hostels.count;
+        }
+    }
+    return 0;
+}
+
+- (NSString *)menu:(DOPDropDownMenu1 *)menu titleForItemsInRowAtIndexPath:(DOPIndexPath1 *)indexPath
+{
+    if (indexPath.column == 0) {
+        if (indexPath.row == 0) {
+            return self.cates[indexPath.item];
+        } else if (indexPath.row == 2){
+            return self.movices[indexPath.item];
+        } else if (indexPath.row == 3){
+            return self.hostels[indexPath.item];
+        }
+    }
+    return nil;
+}
+
+- (NSString *)menu:(DOPDropDownMenu1 *)menu detailTextForRowAtIndexPath:(DOPIndexPath1 *)indexPath
+{
+    if (indexPath.column < 2) {
+        return [@(arc4random()%1000) stringValue];
+    }
+    
+    return nil;
+}
+
+- (NSString *)menu:(DOPDropDownMenu1 *)menu detailTextForItemsInRowAtIndexPath:(DOPIndexPath1 *)indexPath
+{
+    return [@(arc4random()%1000) stringValue];
+}
+
+#pragma mark - DOPDropDownMenuExtendDelegate
+
+- (void)menu:(DOPDropDownMenu1 *)menu didSelectColumn:(NSUInteger)column {
+    // 在这里尝试做动画
+    [UIView animateWithDuration:2. animations:^{
+        NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
+        [_tableView scrollToRowAtIndexPath:indexPath atScrollPosition:UITableViewScrollPositionTop animated:YES];
+    }];
+    NSLog(@"I select column--%ld ", column);
+}
+
+#pragma mark - DOPDropDownMenuDelegate
+
+/**
+ *  点击代理，点击了第column 第row 或者item项，如果 item >=0
+ */
+- (void)menu:(DOPDropDownMenu1 *)menu didSelectRowAtIndexPath:(DOPIndexPath1 *)indexPath {
+    // 在这里尝试做动画
+    [UIView animateWithDuration:2. animations:^{
+        NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
+        [_tableView scrollToRowAtIndexPath:indexPath atScrollPosition:UITableViewScrollPositionTop animated:YES];
+    }];
+    NSLog(@"I select %ld--%ld--%ld ", indexPath.column, indexPath.row, indexPath.item);
 }
 
 @end
