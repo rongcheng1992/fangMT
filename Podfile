f9474aeb6b5ba9acab99f1bcaa1e9135b88ab0de
chengrong03 <chengrong03@meituan.com>
2017-05-10 12:03:06 +0800

    删除多余pod

--- Podfile
+++ Podfile
@@ -6,9 +6,7 @@ use_frameworks!
 target :fangMT do
 	pod 'MJRefresh','3.0.2'
 	pod 'Masonry','1.0.2'
-    pod 'SDWebImage','4.0.0'
-    pod 'Reveal-iOS-SDK', :configurations => ['Debug']
-    pod 'ReactiveCocoa', '2.5.0'
-    pod 'DOPDropDownMenu-Enhanced', '~> 1.0.0'
+        pod 'SDWebImage','4.0.0'
+        pod 'ReactiveCocoa', '2.5.0'
 end
 
--- Pods/Target Support Files/Pods-fangMT/Pods-fangMT-frameworks.sh
+++ Pods/Target Support Files/Pods-fangMT/Pods-fangMT-frameworks.sh
@@ -89,14 +89,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/DOPDropDownMenu-Enhanced/DOPDropDownMenu_Enhanced.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MJRefresh/MJRefresh.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Masonry/Masonry.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ReactiveCocoa/ReactiveCocoa.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/DOPDropDownMenu-Enhanced/DOPDropDownMenu_Enhanced.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MJRefresh/MJRefresh.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Masonry/Masonry.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ReactiveCocoa/ReactiveCocoa.framework"
